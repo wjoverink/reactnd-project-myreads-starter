022ac9b22f01fd94c0112a4bfe758ae906b6ecbb
WJ Overink <willemjan.overink@gmail.com>
2018-02-02 12:58:15 +0100

    chore: structured the files in different directories

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
-import './App.css'
+import './css/App.css'
 import {Route} from 'react-router-dom'
 
 class BooksApp extends React.Component {
