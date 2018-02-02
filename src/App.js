a1752b1ab7dfeaa964d203e2f7f8557f14c3345c
WJ Overink <willemjan.overink@gmail.com>
2018-02-02 08:55:33 +0100

    feat: Added react router, sort-by, form-serialize and escape-string-regexp packages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import {Route} from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
